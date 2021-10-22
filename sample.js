f5d21cfb84298e7188fa66da6e2768b63c2c843c
dai-gi <daisuke.u524@gmail.com>
2021-10-23 06:08:42 +0900

    if文の基本の使い方

--- sample.js
+++ sample.js
@@ -1,7 +1,4 @@
-let age = 20;
-console.log(age ＜ 30);  //比較演算子が全角
-console.log(age <= 15);
-console.log(age > 10);
-console.log(age >= 30);
-console.log(age == 20);
-console.log(age != 20);
+let point = 5;
+if (point >= 5) {
+    console.log("High");
+}
