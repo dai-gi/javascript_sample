5feefeeed24225245f42df9832938b4e83e942ab
dai-gi <daisuke.u524@gmail.com>
2021-10-25 05:56:35 +0900

    簡単なデバッグの方法

--- sample.js
+++ sample.js
@@ -1,6 +1,7 @@
 function count() {
-    for (let num = 1; num < 5; num = num + 1) {
-        if (num == 3) {
+    for (let num = "1"; num < 100; num = num + 1) {
+        debugger;
+        if (num == 5) {
             return;
         }
         console.log(num);
