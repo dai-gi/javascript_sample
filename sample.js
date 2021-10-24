9cb89e1aadac56882594e549204a6f6355aecdc7
dai-gi <daisuke.u524@gmail.com>
2021-10-24 19:55:23 +0900

    繰り返し処理の中のreturnで処理から抜ける

--- sample.js
+++ sample.js
@@ -1,5 +1,10 @@
-function add(n, y) {
-    n + y;
+function count() {
+    for (let num = 1; num < 5; num = num + 1) {
+        if (num == 3) {
+            return;
+        }
+        console.log(num);
+    }
 }
 
-console.log(add(2, 3));
+count();
