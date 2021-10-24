f603cabf04eadac72678af1939e93553cb9ff6f3
dai-gi <daisuke.u524@gmail.com>
2021-10-25 06:02:37 +0900

    イベントハンドラを登録

--- sample.js
+++ sample.js
@@ -1,11 +1,3 @@
-function count() {
-    for (let num = "1"; num < 100; num = num + 1) {
-        debugger;
-        if (num == 5) {
-            return;
-        }
-        console.log(num);
-    }
+function clickAlert() {
+    alert("クリックされました");
 }
-
-count();
