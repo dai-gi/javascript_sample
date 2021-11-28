d39bb9550175557bc6d3c100349cb1b10a9f7a7f
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-11-28 14:05:04 +0900

    ブロックスコープの有効範囲

--- sample.js
+++ sample.js
@@ -1,11 +1,24 @@
-let firstName = "Taro";
-
-function greeting() {
-    let familyName = "DIVE";
-    console.log(familyName, firstName);
+let number = 1;
+{
+    let number = 2;
+    console.log(number);
 }
+console.log(number);
+// ブロック内の出力結果：2
+// ブロック外の出力結果：1
 
-greeting();
-// DIVE Taro
-console.log(familyName);
-// ReferenceError: familyName is not defined
+{
+    let string = "B";
+    console.log(string);
+    console.log(number);
+}
+// ブロック内の出力結果：B
+// ブロック外の出力結果：1
+
+{
+    let string = "A";
+    console.log(string);
+}
+console.log(string);
+// ブロック内の出力結果：A
+// ブロック外の出力結果：ReferenceError: string is not defined
