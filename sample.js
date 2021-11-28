6ed886cff03e2e9e5a6cd24df4e3befd59fd1bd6
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-11-28 23:42:38 +0900

    テンプレートリテラル

--- sample.js
+++ sample.js
@@ -1,25 +1,13 @@
-console.log("A" + "BC");
-console.log("12" + "34");
-// ABC
-// 1234
+// 複数行の文字列を扱う
+let text = `こんにちは。
+今日はいい天気ですね。`;
+console.log(text);
+// こんにちは。
+// 今日はいい天気ですね
 
-console.log(1 + "BC"); // 1は文字列として評価される
-// 1BC
-
-// 変数 + 文字列
-let name = "Alice";
+// 変数や式の結果を文字列の中で使う
+name = "Alice";
 let age = 10;
-console.log(name + "は" + age + "歳です。");
+result = `${name}は${age}歳です。`; // 左の記述方法を使うことによって、+を使わずに変数と文字列を出力することができる
+console.log(result);
 // Aliceは10歳です。
-
-// +（プラス）以外の演算はできない
-console.log("A" * 3);
-console.log("A" / 3);
-// NaN
-// NaN
-
-// 数値に変換できる値は自動で変換して演算される
-console.log("2" * "3");
-console.log("9" / 3);
-// 6
-// 3
