5dc26800ab20ecb7c35fbbfbc650c707d065cd88
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-11-28 14:09:43 +0900

    データ型を確認するメソッド

--- sample.js
+++ sample.js
@@ -1,24 +1,6 @@
-let number = 1;
-{
-    let number = 2;
-    console.log(number);
-}
-console.log(number);
-// ブロック内の出力結果：2
-// ブロック外の出力結果：1
-
-{
-    let string = "B";
-    console.log(string);
-    console.log(number);
-}
-// ブロック内の出力結果：B
-// ブロック外の出力結果：1
-
-{
-    let string = "A";
-    console.log(string);
-}
-console.log(string);
-// ブロック内の出力結果：A
-// ブロック外の出力結果：ReferenceError: string is not defined
+console.log(typeof 1);
+console.log(typeof "Hello,World");
+console.log(typeof true);
+// number
+// string
+// boolean
