ee083bc9b250a36bebbb20b26a9c1ed2a4d7a815
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-16 05:36:50 +0900

    for/of 文

--- javascript/sample.js
+++ javascript/sample.js
@@ -1,13 +1,4 @@
-// 複数行の文字列を扱う
-let text = `こんにちは。
-今日はいい天気ですね。`;
-console.log(text);
-// こんにちは。
-// 今日はいい天気ですね
-
-// 変数や式の結果を文字列の中で使う
-name = "Alice";
-let age = 10;
-result = `${name}は${age}歳です。`; // 左の記述方法を使うことによって、+を使わずに変数と文字列を出力することができる
-console.log(result);
-// Aliceは10歳です。
+const numbers = [1, 2, 3, 4, 5, 6];
+for (const num of numbers) {
+    console.log(num);
+}
