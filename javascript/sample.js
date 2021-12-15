08c13a8d7fe8c07d55aad68e252eb8c4ed14f520
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-16 05:50:29 +0900

    continue

--- javascript/sample.js
+++ javascript/sample.js
@@ -1,10 +1,4 @@
 for(let num = 1; num < 5; num++) {
-    if(num == 3) break;
+    if(num == 3) continue;
     console.log(num);
-}
-
-for(let num = 1; num < 5; num++) {
-    if(num == 4);
-    console.log(num);
-    break
 }
