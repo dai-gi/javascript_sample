328f8434a0ab53d7aaa18890dd3b356050f98db0
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-17 20:18:41 +0900

    Fix: 送料算出プログラム追加の解答例に沿って修正
    sum変数、postage変数、両方の値を更新するif文を記述していた。

--- javascript/sale.js
+++ javascript/sale.js
@@ -10,15 +10,22 @@ function add() {
         number: number,
     };
     purchases.push(purchase);
-    console.log(purchases);
 }
 
 function calc() {
     let sum = 0;
+    let postage; // 条件分岐で値が決まるため定義のみに変更
     for(let index = 0; index < purchases.length; index++) {
         sum += purchases[index].price * purchases[index].number;
+        if(sum == 0 || sum >= 3000) {
+            postage = 0;
+        } else if(sum < 2000) {
+            sum += 500;
+        } else {
+            sum += 250;
+        }
     }
-    window.alert(`合計は${sum}円です`);
+    window.alert(`送料は${postage}円です。合計は${sum + postage}円です`)
     purchases = [];
     priceElement.value = "";
     numberElement.value = "";
