337b666a769da2414fee8af20ea36f37c00723a3
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-20 20:36:57 +0900

    Fix: コードの誤りを修正

--- javascript/sale.js
+++ javascript/sale.js
@@ -15,15 +15,15 @@ function add() {
 function calc() {
     let sum = 0;
     let postage; // 条件分岐で値が決まるため定義のみに変更
-    for(let index = 0; index < purchases.length; index++) {
-        sum += purchases[index].price * purchases[index].number;
-        if(sum == 0 || sum >= 3000) {
-            postage = 0;
-        } else if(sum < 2000) {
-            postage += 500;
-        } else {
-            postage += 250;
-        }
+    for(let i=0; i<purchases.length; i++) {
+        sum += purchases[i].price * purchases[i].number;
+    }
+    if(sum == 0 || sum >= 3000) {
+        postage = 0;
+    } else if(sum < 1000) {
+        postage = 500;
+    } else {
+        postage = 250;
     }
     window.alert(`送料は${postage}円です。合計は${sum + postage}円です`)
     purchases = [];
