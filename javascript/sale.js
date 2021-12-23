74e47aea0586d43aab5da3de131758068582ad55
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-23 21:12:36 +0900

    Improve: 繰り返し構文をイテレーションに変更

--- javascript/sale.js
+++ javascript/sale.js
@@ -9,15 +9,32 @@ function add() {
         price: price,
         number: number,
     };
-    purchases.push(purchase);
+    purchases.push(purchase)
+    window.alert(`${display()}\n小計${subtotal()}円`);
 }
 
-function calc() {
-    let sum = 0;
-    let postage; // 条件分岐で値が決まるため定義のみに変更
-    for(let i=0; i<purchases.length; i++) {
-        sum += purchases[i].price * purchases[i].number;
+function display(){
+    return purchases.map(purchase => {
+        return `${purchase.price}円が${purchase.number}点`
+    }).join("\n");
+}
+
+function subtotal() {
+    return purchases.reduce((prev, purchase) => {
+        return prev + purchase.price * purchase.number
+    }, 0);
     }
+
+function calc() {
+    const sum = subtotal();
+    const postage = calcPostageFromPurchase(sum);
+    window.alert(`小計は${sum}円、送料は${postage}円です。合計は${sum + postage}円です`);
+    purchases = [];
+    priceElement.value = "";
+    numberElement.value = "";
+}
+
+function calcPostageFromPurchase(sum) {
     if(sum == 0 || sum >= 3000) {
         postage = 0;
     } else if(sum < 1000) {
@@ -25,8 +42,4 @@ function calc() {
     } else {
         postage = 250;
     }
-    window.alert(`送料は${postage}円です。合計は${sum + postage}円です`)
-    purchases = [];
-    priceElement.value = "";
-    numberElement.value = "";
 }
