72271d234497e0af08b149e37c835427e2364418
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-27 17:45:02 +0900

    Fix: 追加時に表示されるポップの中身を改修
    同じ金額の場合でも改行されて新たに表示されていた。
    新しく追加れされたオブジェクトかを確認するように改修し解決した。

--- javascript/sale.js
+++ javascript/sale.js
@@ -3,19 +3,29 @@ const numberElement = document.getElementById("number");
 let purchases = [];
 
 function add() {
-    const price = parseInt(priceElement.value);
-    const number = parseInt(numberElement.value);
+    const price = priceElement.value;
+    const number = numberElement.value;
+
     let purchase = {
-        price: price,
-        number: number,
+        price: parseInt(price),
+        number: parseInt(number),
     };
-    purchases.push(purchase)
+
+    const newPurchase = purchases.findIndex((item) => item.price === purchase.price)
+    if (purchases.length < 1 || newPurchase === -1) {
+        purchases.push(purchase);
+    } else {
+        purchases[newPurchase].number += purchase.number;
+    }
+
     window.alert(`${display()}\n小計${subtotal()}円`);
+    priceElement.value = "";
+    numberElement.value = "";
 }
 
-function display(){
+function display() {
     return purchases.map(purchase => {
-        return `${purchase.price}円が${purchase.number}点`
+        return `${purchase.price}円が${purchase.number}点`;
     }).join("\n");
 }
 
@@ -23,23 +33,21 @@ function subtotal() {
     return purchases.reduce((prev, purchase) => {
         return prev + purchase.price * purchase.number
     }, 0);
-    }
+}
 
 function calc() {
     const sum = subtotal();
     const postage = calcPostageFromPurchase(sum);
     window.alert(`小計は${sum}円、送料は${postage}円です。合計は${sum + postage}円です`);
     purchases = [];
-    priceElement.value = "";
-    numberElement.value = "";
 }
 
 function calcPostageFromPurchase(sum) {
-    if(sum == 0 || sum >= 3000) {
-        postage = 0;
-    } else if(sum < 1000) {
-        postage = 500;
+    if (sum == 0 || sum >= 3000) {
+        return 0;
+    } else if (sum < 1000) {
+        return 500;
     } else {
-        postage = 250;
+        return 250;
     }
 }
