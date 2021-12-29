9db9aa7e293b996ec21f5e31b4dc05be2302fc78
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-29 16:49:47 +0900

    Improve: 商品データをidで管理するように改善

--- javascript/sale.js
+++ javascript/sale.js
@@ -1,31 +1,56 @@
-const priceElement = document.getElementById("product");
+const idElement = document.getElementById("product");
 const numberElement = document.getElementById("number");
+const products = [
+    {
+        id: "1",
+        name: "オリジナルブレンド200g",
+        price: 500
+    },
+    {
+        id: "2",
+        name: "オリジナルブレンド500g",
+        price: 900
+    },
+    {
+        id: "3",
+        name: "スペシャルブレンド200g",
+        price: 700
+    },
+    {
+        id: "4",
+        name: "スペシャルブレンド500g",
+        price: 1200
+    }
+]
 let purchases = [];
 
 function add() {
-    const price = priceElement.value;
+    let purchase;
     const number = numberElement.value;
-
-    let purchase = {
-        price: parseInt(price),
-        number: parseInt(number),
-    };
-
-    const newPurchase = purchases.findIndex((item) => item.price === purchase.price)
+    products.forEach(element => {
+        if(element.id === idElement.value) {
+            purchase = {
+                id: element.id,
+                name: element.name,
+                price: element.price,
+                number: parseInt(number)
+            };
+        }
+    })
+    const newPurchase = purchases.findIndex((item) => item.id === purchase.id)
     if (purchases.length < 1 || newPurchase === -1) {
         purchases.push(purchase);
     } else {
         purchases[newPurchase].number += purchase.number;
     }
-
-    window.alert(`${display()}\n小計${subtotal()}円`);
-    priceElement.value = "";
+    window.alert(`${display()}\n\n小計${subtotal()}円`);
+    idElement.value = "";
     numberElement.value = "";
 }
 
 function display() {
     return purchases.map(purchase => {
-        return `${purchase.price}円が${purchase.number}点`;
+        return `${purchase.name} ${purchase.price}円:${purchase.number}点`;
     }).join("\n");
 }
 
@@ -38,7 +63,7 @@ function subtotal() {
 function calc() {
     const sum = subtotal();
     const postage = calcPostageFromPurchase(sum);
-    window.alert(`小計は${sum}円、送料は${postage}円です。合計は${sum + postage}円です`);
+    window.alert(`${display()}\n\n小計は${sum}円、送料は${postage}円です。合計は${sum + postage}円です`);
     purchases = [];
 }
 
