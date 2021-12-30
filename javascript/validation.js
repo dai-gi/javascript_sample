113d336f43f9dac133bcb05df1baa0a75b5b5c04
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2021-12-30 20:07:47 +0900

    Add: EメールとEメール(確認)のバリデーション機能を追加

---
+++ javascript/validation.js
@@ -0,0 +1,14 @@
+function confirmSubmit() {
+  const forms = document.forms;
+  forms[0].onsubmit = function() {
+    if( forms[0].email.value !== forms[0].email_confirm.value) {
+      const element = document.createElement('p');
+      const message = document.createTextNode('Eメールが一致しません');
+      element.appendChild(message);
+      forms[0].appendChild(element);
+      return false;
+    }
+  };
+};
+
+window.onload = confirmSubmit;
