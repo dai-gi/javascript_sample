aae37922c5639206e7bfb66e68dc9762fd69d5b5
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-01-02 20:10:16 +0900

    addEventListener()メソッドに書き換え

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,15 +1,15 @@
-function confirmSubmit() {
-  const forms = document.forms;
-  forms[0].onsubmit = function() {
-    if( forms[0].email.value !== forms[0].email_confirm.value) {
+function emailValidation() {
+  const forms = document.getElementById('form');
+  forms.addEventListener("submit", function() {
+    if( forms.email.value !== forms.email_confirm.value) {
       const element = document.createElement('p');
       const message = document.createTextNode('Eメールが一致しません');
       element.appendChild(message);
-      forms[0].appendChild(element);
+      forms.appendChild(element);
       element.classList.add('alert')
       return false;
     }
-  };
+  });
 };
 
-window.onload = confirmSubmit;
+window.onload = emailValidation;
